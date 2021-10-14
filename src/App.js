9415ecc9bf2e3df488bc73a350660136d1696d1f
hmzsjd <github@hamzasajid.com>
2021-10-14 14:59:49 -0700

    working api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+/**
+ * 
+ * API DATA:
+ * user.location -> state
+ * user.uuid
+ * user.picture.medium
+ * user.name -> user.first + user.last
+ * user.email
+ * 
+ * user.posts -> stretch
+ * 
+ */
+
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://randomuser.me/api/?results=5")
+    .then(res => {
+      console.log(res.data.results);
+    })
+    .catch(err => console.error(err));
+  }, [])
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CaseyBook</h1>
+      <h2>Like Facebook</h2>
+      <p>Coming Soon...</p>
     </div>
   );
 }
